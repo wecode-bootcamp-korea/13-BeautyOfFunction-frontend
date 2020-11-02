96922941203aa294b0b180dcd2467678dac8b9e6
이지영 <leejyart@leejyart.local>
2020-11-02 17:48:32 +0900

    first commit

---
+++ src/pages/Detailpage/Detailpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Detailpage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default Detailpage;
---
+++ src/pages/Mypage/Mypage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Mypage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default Mypage;
