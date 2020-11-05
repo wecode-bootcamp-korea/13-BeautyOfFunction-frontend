5c72a936ccb5b79913591ced1953a5a7370de385
김보경 <akbk1004@gmail.com>
2020-11-05 17:24:38 +0900

    Modify: 코드 전반적인 수정진행

--- src/Routes.js
+++ src/Routes.js
@@ -1,33 +1,36 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// eslint-disable-next-line prettier/prettier
+import React, {Component} from "react";
+// eslint-disable-next-line prettier/prettier
+import {BrowserRouter as Router, Route} from "react-router-dom";
+//Switch
 
-import MainPage from "./pages/Mainpage/Mainpage";
-import About from "./pages/About/About";
-import Cart from "./pages/Cart/Cart";
-import Login from "./pages/Login/Login";
+// import MainPage from "./pages/Mainpage/Mainpage";
+// import About from "./pages/About/About";
+// import Cart from "./pages/Cart/Cart";
+// import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
-import Mypage from "./pages/Mypage/Mypage";
-import DetailPage from "./pages/Detailpage/Detailpage";
-import Quiz from "./pages/Quiz/Quiz";
-import Reviewpage from "./pages/Reviewpage/Reviewpage";
-import Nav from "./components/Nav/Nav";
+// import Mypage from "./pages/Mypage/Mypage";
+// import DetailPage from "./pages/Detailpage/Detailpage";
+// import Quiz from "./pages/Quiz/Quiz";
+// import Reviewpage from "./pages/Reviewpage/Reviewpage";
+// import Nav from "./components/Nav/Nav";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/About" component={About} />
+        {/* <Nav /> */}
+        {/* <Switch> */}
+        {/* <Route exact path="/About" component={About} />
           <Route exact path="/Cart" component={Cart} />
-          <Route exact path="/Login" component={Login} />
-          <Route exact path="/Signup" component={Signup} />
-          <Route exact path="/MainPage" component={MainPage} />
+          <Route exact path="/Login" component={Login} /> */}
+        <Route exact path="/" component={Signup} />
+        {/* <Route exact path="/MainPage" component={MainPage} />
           <Route exact path="/Mypage" component={Mypage} />
           <Route exact path="/detail" component={DetailPage} />
           <Route exact path="/Quiz" component={Quiz} />
           <Route exact path="/Reviewpage" component={Reviewpage} />
-        </Switch>
+        </Switch> */}
         {/* <Footer /> */}
       </Router>
     );
