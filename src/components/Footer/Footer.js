c291f1940367039970ca7cc09129d72aa240716c
Corete <fanthell95@gmail.com>
2020-11-10 22:06:14 +0900

    Nav/Footer 리팩토링 완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
+import "./Footer.scss";
 
 class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      img: [
+        {
+          alt: "facebook",
+          img: "./images/facebook.svg",
+        },
+        {
+          alt: "instagram",
+          img: "./images/instagram.svg",
+        },
+        {
+          alt: "pinterest",
+          img: "./images/pinterest.svg",
+        },
+        {
+          alt: "youtube",
+          img: "./images/youtube.svg",
+        },
+        {
+          alt: "twitter",
+          img: "./images/twitter.svg",
+        },
+      ],
+    };
+  }
   render() {
     return (
-      <div>
-        <h1>hi</h1>
-      </div>
+      <footer>
+        <div className="footerContainer">
+          <div className="footerLeft">
+            {this.state.img.map((img) => {
+              return <img alt={img.alt} src={img.img}></img>;
+            })}
+          </div>
+          <div className="footerRight">
+            <span>contact</span>
+            <span>careers</span>
+            <span>privacy</span>
+            <span>terms</span>
+          </div>
+        </div>
+      </footer>
     );
   }
 }
