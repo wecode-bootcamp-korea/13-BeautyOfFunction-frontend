673272ef20bed44e622a3172da8f66502b85fca0
Corete <fanthell95@gmail.com>
2020-11-10 22:06:14 +0900

    Nav/Footer 리팩토링 완료

--- src/Routes.js
+++ src/Routes.js
@@ -13,6 +13,8 @@ import Mypage from "./pages/Mypage/Mypage";
 import DetailPage from "./pages/Detailpage/Detailpage";
 import Quiz from "./pages/Quiz/Quiz";
 import Reviewpage from "./pages/Reviewpage/Reviewpage";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
 
 class Routes extends Component {
   render() {
@@ -23,12 +25,14 @@ class Routes extends Component {
           <Route exact path="/Cart" component={Cart} />
           <Route exact path="/Login" component={Login} />
           <Route exact path="/Signup" component={Signup} />
-          {/* <Route exact path="/MainPage" component={MainPage} /> */}
+          <Route exact path="/MainPage" component={MainPage} />
           <Route exact path="/Mypage" component={Mypage} />
           <Route exact path="/MainPage" component={MainPage} />
           <Route exact path="/detail" component={DetailPage} />
           <Route exact path="/Quiz" component={Quiz} />
           <Route exact path="/Reviewpage" component={Reviewpage} />
+          <Route exact path="/Nav" component={Nav} />
+          <Route exact path="/Footer" component={Footer} />
         </Switch>
       </Router>
     );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,103 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.scss";
 
 class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showQuiz: false,
+      showProducts: false,
+    };
+  }
+
+  openQuiz = () => {
+    this.setState({ showQuiz: !this.state.showQuiz });
+  };
+
+  openProducts = () => {
+    this.setState({ showProducts: !this.state.showProducts });
+  };
+
   render() {
     return (
-      <div>
-        <h1>hi</h1>
+      <div className="nav">
+        <div className="navbar">
+          <div className="navbarMargin">
+            <div className="navItem quiz">
+              <a className="subNavItem" onClick={this.openQuiz}>
+                quiz
+                <img
+                  className="arrowDown"
+                  alt=""
+                  src="./images/arrowdown.png"
+                ></img>
+              </a>
+              {this.state.showQuiz && (
+                <div className="Toggle">
+                  <div className="Box">
+                    <div className="quizHair">
+                      <div>
+                        <img alt="quizImg" src="./images/comb.svg"></img>
+                        <span>#1 rated custom hair care</span>
+                      </div>
+                      <a>take the hair quiz</a>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="navItem products">
+              <a className="subNavItem" onClick={this.openProducts}>
+                products
+                <img
+                  alt="navArrowDown"
+                  className="arrowDown"
+                  src="./images/arrowdown.png"
+                ></img>
+              </a>
+              {this.state.showProducts && (
+                <div className="Toggle">
+                  <div className="Box">
+                    <div className="productsHair">
+                      <div className="hairCare">
+                        <h1>HAIR CARE</h1>
+                        <ul>
+                          <li>shampoo + conditioner</li>
+                          <li>purple shampoo</li>
+                          <li>leave-inr</li>
+                          <li>hair maskr</li>
+                          <li>hair serum</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <a className="subNavItem">reviews</a>
+            <div>
+              <a className="subNavItem">
+                <img
+                  alt="navLogo"
+                  className="navLogo"
+                  src="./images/MainLogoNF.png"
+                ></img>
+              </a>
+            </div>
+            <div className="navItem about">
+              <Link to="/About" className="subNavItem" onClick={this.openAbout}>
+                about
+              </Link>
+            </div>
+            <Link to="/Signup" className="subNavItem">
+              account
+            </Link>
+            <Link to="/Cart" className="subNavItem">
+              cart
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
