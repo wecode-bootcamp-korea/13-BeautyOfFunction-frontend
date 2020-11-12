275f20346dc072a0dce960dd9326939dbdc79471
이지영 <leejyart@leejyart.local>
2020-11-12 18:43:56 +0900

    Fix: conflict resolved

---
+++ src/pages/Reviewpage/ReviewpageData.js
@@ -0,0 +1,162 @@
+const ReviewData = {
+  STARTABLE: {
+    "SHAMPOO & CONDITIONER": [
+      { stars: "5 stars", cnt: 52219, total: 63228, starRating: 3.1 },
+      { stars: "4 stars", cnt: 5143, total: 63228 },
+      { stars: "3 stars", cnt: 2068, total: 63228 },
+      { stars: "2 stars", cnt: 1702, total: 63228 },
+      { stars: "1 stars", cnt: 2096, total: 63228 },
+    ],
+
+    "PURPLE SHAMPOO": [
+      { stars: "5 stars", cnt: 52219, total: 63228, starRating: 4.5 },
+      { stars: "4 stars", cnt: 5143, total: 63228 },
+      { stars: "3 stars", cnt: 2068, total: 63228 },
+      { stars: "2 stars", cnt: 5143, total: 63228 },
+      { stars: "1 stars", cnt: 2096, total: 63228 },
+    ],
+
+    "LEAVE-IN": [
+      { stars: "5 stars", cnt: 152, total: 3265, starRating: 2.5 },
+      { stars: "4 stars", cnt: 341, total: 3265 },
+      { stars: "3 stars", cnt: 210, total: 3265 },
+      { stars: "2 stars", cnt: 126, total: 3265 },
+      { stars: "1 stars", cnt: 2436, total: 3265 },
+    ],
+    "HAIR MASK": [
+      { stars: "5 stars", cnt: 2468, total: 3152, starRating: 4.9 },
+      { stars: "4 stars", cnt: 319, total: 3152 },
+      { stars: "3 stars", cnt: 150, total: 3152 },
+      { stars: "2 stars", cnt: 91, total: 3152 },
+      { stars: "1 stars", cnt: 124, total: 3152 },
+    ],
+    "HAIR SERUM": [
+      { stars: "5 stars", cnt: 76, total: 2711, starRating: 1.0 },
+      { stars: "4 stars", cnt: 307, total: 2711 },
+      { stars: "3 stars", cnt: 150, total: 2711 },
+      { stars: "2 stars", cnt: 70, total: 2711 },
+      { stars: "1 stars", cnt: 2108, total: 2711 },
+    ],
+  },
+
+  review_ratings: [
+    {
+      // image: “https://i.ibb.co/QcNmrhJ/serum.png”,
+      total: 10,
+      average: 4.3,
+      rating_five: 4,
+      rating_four: 5,
+      rating_three: 1,
+      rating_two: 0,
+      rating_one: 0,
+    },
+  ],
+
+  review_list: [
+    {
+      name: "Kristal",
+      status: "veryfied buyer",
+      created_at: "10/14/10",
+      rating: 5,
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+    },
+    {
+      name: "Kristal",
+      status: "veryfied buyer",
+      rating: 5,
+      created_at: "10/14/10",
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+    },
+    {
+      name: "Kristal",
+      status: "veryfied buyer",
+      rating: 5,
+      created_at: "10/14/10",
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+    },
+    {
+      name: "Kristal",
+      status: "veryfied buyer",
+      rating: 5,
+      created_at: "10/14/10",
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+    },
+    {
+      name: "Kristal",
+      rating: 5,
+      status: "veryfied buyer",
+      created_at: "10/14/10",
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+    },
+    {
+      name: "Kristal",
+      rating: 5,
+      status: "veryfied buyer",
+      created_at: "10/14/10",
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+    },
+    {
+      name: "Kristal",
+      status: "veryfied buyer",
+      created_at: "10/14/10",
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+      rating: 5,
+    },
+    {
+      name: "Kristal",
+      status: "veryfied buyer",
+      rating: 5,
+      created_at: "10/14/10",
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+    },
+    {
+      name: "Kristal",
+      rating: 5,
+      status: "veryfied buyer",
+      created_at: "10/14/10",
+      title: "Works perfectly for my hair",
+      comment: "Works great and reduces frizz in my hair!",
+    },
+  ],
+
+  FILTER: [
+    {
+      id: 0,
+      key: "Hair Type",
+      icon: "▾",
+      items: ["All", "Coily", "Curly", "Straight", "Wavy"],
+      toggle: false,
+    },
+    {
+      id: 1,
+      key: "Hair Structure",
+      icon: "▾",
+      items: ["All", "Coarse", "Fine", "Medium"],
+      toggle: false,
+    },
+    {
+      id: 2,
+      key: "Scalp Moisture",
+      icon: "▾",
+      items: ["All", "Dry", "Normal", "Oily"],
+      toggle: false,
+    },
+    {
+      id: 3,
+      key: "Filtered Number",
+      icon: "▾",
+      items: [5, 10],
+      toggle: false,
+    },
+  ],
+};
+
+export default ReviewData;
